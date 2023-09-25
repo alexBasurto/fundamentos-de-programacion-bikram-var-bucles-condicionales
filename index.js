1557b7ad59ab652a3cb1a9450fa7e62cfc1c7fe0
alexBasurto <alexbasurto1@gmail.com>
2023-09-25 09:48:49 +0200

    final

--- index.js
+++ index.js
@@ -69,16 +69,31 @@ let seisNoEsNueve = 6 !== 9;
 let booleanoMix2 = variableValorNumerico > 0 || variableValorNumerico < (MiNumeroFav * TAU)
 
 //20
-
+let valorSuma = MiNumeroFav + variableValorNumerico;
 
 //21
-
+let valorResta = MiNumeroFav - variableValorNumerico;
 
 //22
-
+let valorMultiplicacion = MiNumeroFav * variableValorNumerico;
 
 //23
-
+let valorDivision = MiNumeroFav / 3;
 
 //24
+let contarHasta10 = 0;
+while (contarHasta10 < 10) {contarHasta10++};
+
+//25
+let preI = 0;
+let preJ = 0;
+for (let i = 0; i < 11; i++){preI += ++preJ};
+
+//26
+let sumaImpares = 0;
+for (let i = 0; i < 10; i++){
+    if (i%2 != 0){
+        sumaImpares += i;
+    }
+};
 
